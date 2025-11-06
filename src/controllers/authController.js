f965a63074ae58dfce808c1fd1f27d4d5efd2df8
Joyel <Varghesejoyel71@gmail.com>
2025-11-06 22:33:51 +0530

    Refactor code: updated signup logic

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -15,7 +15,7 @@ const requestOtp = async (req, res) => {
 
     const otp = crypto.randomInt(100000, 999999).toString();
     user.resetOtp = otp;
-    user.resetOtpExpires = Date.now() + 5 * 60 * 1000; // 5 min
+    user.resetOtpExpires = Date.now() + 5 * 60 * 1000; 
     await user.save();
 
     await sendOtpEmail(email, otp);
