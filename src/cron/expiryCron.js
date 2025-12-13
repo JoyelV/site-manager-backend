605889d8d4a4645d7863095fd139c55e07979d9f
Joyel <Varghesejoyel71@gmail.com>
2025-12-13 20:25:47 +0530

    Cron job 8.30pm

--- src/cron/expiryCron.js
+++ src/cron/expiryCron.js
@@ -22,7 +22,7 @@ const startExpiryCron = () => {
 
   // RUN EVERYDAY AT 08 PM IST
   cron.schedule(
-    "10 20 * * *",
+    "30 20 * * *",
     async () => {
       console.log("⚡ Cron Triggered at:", new Date().toLocaleString("en-IN", {
         timeZone: "Asia/Kolkata",
